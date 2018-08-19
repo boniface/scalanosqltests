d248ca4c6642bcc5e78dbe11fb3bbe0a7590f071
boniface <boniface@kabaso.com>
2018-08-19 19:58:43 +0200

    updated sbt

--- app/repository/mongodb/MongodbRepository.scala
+++ app/repository/mongodb/MongodbRepository.scala
@@ -1,5 +1,32 @@
 package repository.mongodb
 
-class MongodbRepository {
 
+import domain.Person
+import org.mongodb.scala._
+import repository.Repository
+import scala.collection.JavaConverters._
+
+class MongodbRepository extends Repository{
+
+  val settings: MongoClientSettings = MongoClientSettings.builder()
+    .applyToClusterSettings(b => b.hosts(List(new ServerAddress("localhost")).asJava))
+    .build()
+  val mongoClient: MongoClient = MongoClient(settings)
+
+  val database: MongoDatabase = mongoClient.getDatabase("db")
+  val collection: MongoCollection[Document] = database.getCollection("person")
+
+  override def create(person: Person): Person = {
+    val doc: Document = Document("_id" -> person.id, "name" -> person.name)
+    val resr = collection.insertOne(doc)
+
+  }
+
+  override def update(person: Person): Person = ???
+
+  override def delete(person: Person): Boolean = ???
+
+  override def read(id: String): Person = ???
+
+  override def readAll: List[Person] = ???
 }
