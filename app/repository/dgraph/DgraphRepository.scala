8313a8f268a13c9c80db34d328738a657eff815e
boniface <boniface@kabaso.com>
2018-08-20 21:30:33 +0200

    updated sbt

--- app/repository/dgraph/DgraphRepository.scala
+++ app/repository/dgraph/DgraphRepository.scala
@@ -1,21 +1,31 @@
 package repository.dgraph
 
+import java.util
+
 import domain.Person
+import io.dgraph.DgraphProto.Operation
+import io.dgraph.{DgraphClient, DgraphGrpc}
+import io.grpc.{ManagedChannel, ManagedChannelBuilder}
 import repository.Repository
-import io.dgraph.DgraphClient
-import io.dgraph.DgraphGrpc
-import io.grpc.ManagedChannel
-import io.grpc.ManagedChannelBuilder
-import java.util.Collections
-import collection.JavaConverters._
 
-class DgraphRepository extends Repository{
+import scala.collection.JavaConverters._
+
+class DgraphRepository extends Repository {
 
 
+  val channel: ManagedChannel = ManagedChannelBuilder
+    .forAddress("localhost", 9080)
+    .usePlaintext(true)
+    .build
+  val stub: DgraphGrpc.DgraphBlockingStub = DgraphGrpc.newBlockingStub(channel)
+  val stubs: util.List[DgraphGrpc.DgraphBlockingStub] = List[DgraphGrpc.DgraphBlockingStub](stub).asJava
+  val dgraphClient: DgraphClient = new DgraphClient(stubs)
 
-  val channel: ManagedChannel = ManagedChannelBuilder.forAddress("localhost", 9080).usePlaintext(true).build
-  val stub: DgraphGrpc.DgraphStub = DgraphGrpc.newStub(channel)
-  val dgraphClient: DgraphClient = new DgraphClient(Collections.singletonList(stub).asScala)
+  val schema = " id:   string   @index(exact) .\n" +
+    " name: string   @index(int)  .\n"
+
+  val op = Operation.newBuilder().setSchema(schema).build()
+  dgraphClient.alter(op)
 
 
   override def create(person: Person): Person = ???
@@ -27,4 +37,5 @@ class DgraphRepository extends Repository{
   override def read(id: String): Person = ???
 
   override def readAll: List[Person] = ???
+
 }
